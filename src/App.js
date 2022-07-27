364bb264b63f51b3a6c96467f6cffb20830f3ad3
Mehdi mansouri <mhd67g@gmail.com>
2022-07-27 19:07:30 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h4>HAllo World!</h4>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+        <h1>Mehdi mansouri varnousfaderani</h1>
+        );
+    }
 }
-
 export default App;
