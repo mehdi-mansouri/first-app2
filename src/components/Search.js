03b7b46f763c2e0b614d41c141e792900efc047b
Mehdi mansouri <mhd67g@gmail.com>
2022-07-30 17:35:53 +0200

    const und down und up Button

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import Styles from './Search.module.css';
 
 class Search extends Component {
+    constructor(){
+        super();
+        this.state={
+            text:""
+        }
+    }
+    chaneHandler =event=>{
+        this.setState({
+            text:event.target.value,
+        })
+    }
     render() {
         return (
             <div className={Styles.container}>
+               
                 <p>Search What you Want</p>
-                <input placeholder='Search ...'/>
+                <div>
+                    <input type="text" value={this.state.text} onChange={this.chaneHandler} placeholder='Search ...'/>
+                    <br/>
+                    <br/>
+                    <span>{this.state.text} </span>
+                </div> 
+                
             </div>
         );
     }
--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 
-import Styles from './card.module.css'
+import Styles from './card.module.css';
+import Down from "../images/down.svg";
+import up from "../images/up.svg";
 class card extends Component {
-    
+    constructor(){
+        super()
+        this.state={
+            counter:0
+        }
+    }
+    downHandler=()=>{
+        if (this.state.counter>0) {
+            this.setState(prevState=>({
+                counter:prevState.counter -1
+            }))
+        }
+       
+    }
+    upHandler=()=>{
+        this.setState(prevState=>({
+            counter:prevState.counter +1
+        }))
+    }
     render() {
         const {image ,name ,cost} = this.props;
+        const { counter } = this.state;
         return (
             <div className={Styles.container}>
-                <img src={image} alt='Picture'/>
+                <img src={image} alt='arrow'/>
                 <h3>{name}</h3>
-                <p>{cost}</p>
+                <p>{cost}{counter?`* ${counter} = ${counter * Number(cost.split("$")[0])} $`:""}</p>
+                <div className={Styles.counter}>
+                    <img className={!counter && Styles.deactive} src={Down} alt="Arrow" onClick={this.downHandler}/>
+                    <span>{counter} </span>
+                    <img src={up} alt="Arrow" onClick={this.upHandler}/>
+
+                </div>
             </div>
         );
     }
